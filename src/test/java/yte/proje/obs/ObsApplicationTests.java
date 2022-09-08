10d681a7637fb63b62010e93b5d867eb5f6e9b8f
Gülnihal Mercanoğlu <gulnihal@907a8e1d.drm.metu.edu.tr>
2022-09-08 09:54:08 +0300

    Initial commit

---
+++ src/test/java/yte/proje/obs/ObsApplicationTests.java
@@ -0,0 +1,13 @@
+package yte.proje.obs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
